38ed759ba479f4f48a278e0f32cb7c11806ea527
Alan Potter <alan.potter@calnexsol.com>
2014-10-15 17:09:27 +0100

    Update

--- Wordpress/Form1.Designer.cs
+++ Wordpress/Form1.Designer.cs
@@ -31,23 +31,23 @@
             this.btnGet = new System.Windows.Forms.Button();
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tpMain = new System.Windows.Forms.TabPage();
-            this.tpAuth = new System.Windows.Forms.TabPage();
+            this.rtResponse = new System.Windows.Forms.RichTextBox();
+            this.btnCompleteAuth = new System.Windows.Forms.Button();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.browser1 = new System.Windows.Forms.WebBrowser();
+            this.btnAuthorize = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.tbClientName = new System.Windows.Forms.TextBox();
-            this.tbClientID = new System.Windows.Forms.TextBox();
-            this.tbClientSecret = new System.Windows.Forms.TextBox();
-            this.tbRedirectUri = new System.Windows.Forms.TextBox();
             this.tbCode = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.btnAuthorize = new System.Windows.Forms.Button();
-            this.browser1 = new System.Windows.Forms.WebBrowser();
-            this.btnDone = new System.Windows.Forms.Button();
-            this.btnCompleteAuth = new System.Windows.Forms.Button();
-            this.rtResponse = new System.Windows.Forms.RichTextBox();
+            this.tbRedirectUri = new System.Windows.Forms.TextBox();
+            this.tbClientSecret = new System.Windows.Forms.TextBox();
+            this.tbClientID = new System.Windows.Forms.TextBox();
+            this.tbClientName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tpAuth = new System.Windows.Forms.TabPage();
             this.tabControl.SuspendLayout();
             this.tpMain.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -93,15 +93,54 @@
             this.tpMain.Text = "Main";
             this.tpMain.UseVisualStyleBackColor = true;
             // 
-            // tpAuth
+            // rtResponse
             // 
-            this.tpAuth.Location = new System.Drawing.Point(4, 22);
-            this.tpAuth.Name = "tpAuth";
-            this.tpAuth.Padding = new System.Windows.Forms.Padding(3);
-            this.tpAuth.Size = new System.Drawing.Size(864, 323);
-            this.tpAuth.TabIndex = 1;
-            this.tpAuth.Text = "Authorization";
-            this.tpAuth.UseVisualStyleBackColor = true;
+            this.rtResponse.Location = new System.Drawing.Point(578, 148);
+            this.rtResponse.Name = "rtResponse";
+            this.rtResponse.ReadOnly = true;
+            this.rtResponse.Size = new System.Drawing.Size(267, 402);
+            this.rtResponse.TabIndex = 5;
+            this.rtResponse.Text = "";
+            // 
+            // btnCompleteAuth
+            // 
+            this.btnCompleteAuth.Location = new System.Drawing.Point(578, 83);
+            this.btnCompleteAuth.Name = "btnCompleteAuth";
+            this.btnCompleteAuth.Size = new System.Drawing.Size(75, 23);
+            this.btnCompleteAuth.TabIndex = 4;
+            this.btnCompleteAuth.Text = "Complete";
+            this.btnCompleteAuth.UseVisualStyleBackColor = true;
+            this.btnCompleteAuth.Click += new System.EventHandler(this.btnCompleteAuth_Click);
+            // 
+            // btnDone
+            // 
+            this.btnDone.Enabled = false;
+            this.btnDone.Location = new System.Drawing.Point(578, 50);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(75, 23);
+            this.btnDone.TabIndex = 3;
+            this.btnDone.Text = "Got It";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // browser1
+            // 
+            this.browser1.Location = new System.Drawing.Point(7, 148);
+            this.browser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser1.Name = "browser1";
+            this.browser1.ScriptErrorsSuppressed = true;
+            this.browser1.Size = new System.Drawing.Size(545, 415);
+            this.browser1.TabIndex = 2;
+            // 
+            // btnAuthorize
+            // 
+            this.btnAuthorize.Location = new System.Drawing.Point(578, 17);
+            this.btnAuthorize.Name = "btnAuthorize";
+            this.btnAuthorize.Size = new System.Drawing.Size(75, 23);
+            this.btnAuthorize.TabIndex = 1;
+            this.btnAuthorize.Text = "Authorize";
+            this.btnAuthorize.UseVisualStyleBackColor = true;
+            this.btnAuthorize.Click += new System.EventHandler(this.btnAuthorize_Click);
             // 
             // groupBox1
             // 
@@ -121,90 +160,6 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "App Info";
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Client Name";
-            // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Client ID";
-            // 
-            // label3
-            // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 62);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Client Secret";
-            // 
-            // label4
-            // 
-            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 83);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Redirect URL";
-            // 
-            // tbClientName
-            // 
-            this.tbClientName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbClientName.Location = new System.Drawing.Point(95, 17);
-            this.tbClientName.Name = "tbClientName";
-            this.tbClientName.Size = new System.Drawing.Size(444, 20);
-            this.tbClientName.TabIndex = 4;
-            this.tbClientName.Text = "FeaturedImageUpdater";
-            // 
-            // tbClientID
-            // 
-            this.tbClientID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbClientID.Location = new System.Drawing.Point(95, 38);
-            this.tbClientID.Name = "tbClientID";
-            this.tbClientID.Size = new System.Drawing.Size(444, 20);
-            this.tbClientID.TabIndex = 5;
-            this.tbClientID.Text = "37262";
-            // 
-            // tbClientSecret
-            // 
-            this.tbClientSecret.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbClientSecret.Location = new System.Drawing.Point(95, 59);
-            this.tbClientSecret.Name = "tbClientSecret";
-            this.tbClientSecret.Size = new System.Drawing.Size(444, 20);
-            this.tbClientSecret.TabIndex = 6;
-            this.tbClientSecret.Text = "T9G9QgHVgmdrDgbP3ruTJvrMAcIid8Z1DmvJyXpYAokJRXBZ1oz78QeKMZhCaCpI";
-            // 
-            // tbRedirectUri
-            // 
-            this.tbRedirectUri.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbRedirectUri.Location = new System.Drawing.Point(95, 80);
-            this.tbRedirectUri.Name = "tbRedirectUri";
-            this.tbRedirectUri.Size = new System.Drawing.Size(444, 20);
-            this.tbRedirectUri.TabIndex = 7;
-            this.tbRedirectUri.Text = "http://project365.thepotters.org";
             // 
             // tbCode
             // 
@@ -226,53 +181,99 @@
             this.label5.TabIndex = 8;
             this.label5.Text = "Code";
             // 
-            // btnAuthorize
+            // tbRedirectUri
             // 
-            this.btnAuthorize.Location = new System.Drawing.Point(578, 17);
-            this.btnAuthorize.Name = "btnAuthorize";
-            this.btnAuthorize.Size = new System.Drawing.Size(75, 23);
-            this.btnAuthorize.TabIndex = 1;
-            this.btnAuthorize.Text = "Authorize";
-            this.btnAuthorize.UseVisualStyleBackColor = true;
-            this.btnAuthorize.Click += new System.EventHandler(this.btnAuthorize_Click);
+            this.tbRedirectUri.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRedirectUri.Location = new System.Drawing.Point(95, 80);
+            this.tbRedirectUri.Name = "tbRedirectUri";
+            this.tbRedirectUri.Size = new System.Drawing.Size(444, 20);
+            this.tbRedirectUri.TabIndex = 7;
+            this.tbRedirectUri.Text = "http://project365.thepotters.org";
             // 
-            // browser1
+            // tbClientSecret
             // 
-            this.browser1.Location = new System.Drawing.Point(7, 148);
-            this.browser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.browser1.Name = "browser1";
-            this.browser1.Size = new System.Drawing.Size(545, 415);
-            this.browser1.TabIndex = 2;
+            this.tbClientSecret.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClientSecret.Location = new System.Drawing.Point(95, 59);
+            this.tbClientSecret.Name = "tbClientSecret";
+            this.tbClientSecret.Size = new System.Drawing.Size(444, 20);
+            this.tbClientSecret.TabIndex = 6;
+            this.tbClientSecret.Text = "T9G9QgHVgmdrDgbP3ruTJvrMAcIid8Z1DmvJyXpYAokJRXBZ1oz78QeKMZhCaCpI";
             // 
-            // btnDone
+            // tbClientID
             // 
-            this.btnDone.Enabled = false;
-            this.btnDone.Location = new System.Drawing.Point(578, 50);
-            this.btnDone.Name = "btnDone";
-            this.btnDone.Size = new System.Drawing.Size(75, 23);
-            this.btnDone.TabIndex = 3;
-            this.btnDone.Text = "Got It";
-            this.btnDone.UseVisualStyleBackColor = true;
-            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            this.tbClientID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClientID.Location = new System.Drawing.Point(95, 38);
+            this.tbClientID.Name = "tbClientID";
+            this.tbClientID.Size = new System.Drawing.Size(444, 20);
+            this.tbClientID.TabIndex = 5;
+            this.tbClientID.Text = "37262";
             // 
-            // btnCompleteAuth
+            // tbClientName
             // 
-            this.btnCompleteAuth.Location = new System.Drawing.Point(578, 83);
-            this.btnCompleteAuth.Name = "btnCompleteAuth";
-            this.btnCompleteAuth.Size = new System.Drawing.Size(75, 23);
-            this.btnCompleteAuth.TabIndex = 4;
-            this.btnCompleteAuth.Text = "Complete";
-            this.btnCompleteAuth.UseVisualStyleBackColor = true;
-            this.btnCompleteAuth.Click += new System.EventHandler(this.btnCompleteAuth_Click);
+            this.tbClientName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClientName.Location = new System.Drawing.Point(95, 17);
+            this.tbClientName.Name = "tbClientName";
+            this.tbClientName.Size = new System.Drawing.Size(444, 20);
+            this.tbClientName.TabIndex = 4;
+            this.tbClientName.Text = "FeaturedImageUpdater";
             // 
-            // rtResponse
+            // label4
             // 
-            this.rtResponse.Location = new System.Drawing.Point(578, 148);
-            this.rtResponse.Name = "rtResponse";
-            this.rtResponse.ReadOnly = true;
-            this.rtResponse.Size = new System.Drawing.Size(267, 402);
-            this.rtResponse.TabIndex = 5;
-            this.rtResponse.Text = "";
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Redirect URL";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Client Secret";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Client ID";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client Name";
+            // 
+            // tpAuth
+            // 
+            this.tpAuth.Location = new System.Drawing.Point(4, 22);
+            this.tpAuth.Name = "tpAuth";
+            this.tpAuth.Padding = new System.Windows.Forms.Padding(3);
+            this.tpAuth.Size = new System.Drawing.Size(864, 569);
+            this.tpAuth.TabIndex = 1;
+            this.tpAuth.Text = "Authorization";
+            this.tpAuth.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
