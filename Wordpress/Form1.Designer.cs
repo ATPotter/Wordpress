c8e36654bfa5aee5878f063223bef62a866f32ad
Alan Potter <alan.potter@calnexsol.com>
2014-10-15 16:33:05 +0100

    Try to perform authorisation

--- Wordpress/Form1.Designer.cs
+++ Wordpress/Form1.Designer.cs
@@ -29,12 +29,34 @@
         private void InitializeComponent()
         {
             this.btnGet = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tpMain = new System.Windows.Forms.TabPage();
+            this.tpAuth = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbClientName = new System.Windows.Forms.TextBox();
+            this.tbClientID = new System.Windows.Forms.TextBox();
+            this.tbClientSecret = new System.Windows.Forms.TextBox();
+            this.tbRedirectUri = new System.Windows.Forms.TextBox();
+            this.tbCode = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAuthorize = new System.Windows.Forms.Button();
+            this.browser1 = new System.Windows.Forms.WebBrowser();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.btnCompleteAuth = new System.Windows.Forms.Button();
+            this.rtResponse = new System.Windows.Forms.RichTextBox();
+            this.tabControl.SuspendLayout();
+            this.tpMain.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnGet
             // 
             this.btnGet.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnGet.Location = new System.Drawing.Point(13, 368);
+            this.btnGet.Location = new System.Drawing.Point(13, 614);
             this.btnGet.Name = "btnGet";
             this.btnGet.Size = new System.Drawing.Size(75, 23);
             this.btnGet.TabIndex = 0;
@@ -42,14 +64,229 @@
             this.btnGet.UseVisualStyleBackColor = true;
             this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
             // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tpMain);
+            this.tabControl.Controls.Add(this.tpAuth);
+            this.tabControl.Location = new System.Drawing.Point(13, 13);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(872, 595);
+            this.tabControl.TabIndex = 1;
+            // 
+            // tpMain
+            // 
+            this.tpMain.Controls.Add(this.rtResponse);
+            this.tpMain.Controls.Add(this.btnCompleteAuth);
+            this.tpMain.Controls.Add(this.btnDone);
+            this.tpMain.Controls.Add(this.browser1);
+            this.tpMain.Controls.Add(this.btnAuthorize);
+            this.tpMain.Controls.Add(this.groupBox1);
+            this.tpMain.Location = new System.Drawing.Point(4, 22);
+            this.tpMain.Name = "tpMain";
+            this.tpMain.Padding = new System.Windows.Forms.Padding(3);
+            this.tpMain.Size = new System.Drawing.Size(864, 569);
+            this.tpMain.TabIndex = 0;
+            this.tpMain.Text = "Main";
+            this.tpMain.UseVisualStyleBackColor = true;
+            // 
+            // tpAuth
+            // 
+            this.tpAuth.Location = new System.Drawing.Point(4, 22);
+            this.tpAuth.Name = "tpAuth";
+            this.tpAuth.Padding = new System.Windows.Forms.Padding(3);
+            this.tpAuth.Size = new System.Drawing.Size(864, 323);
+            this.tpAuth.TabIndex = 1;
+            this.tpAuth.Text = "Authorization";
+            this.tpAuth.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbCode);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbRedirectUri);
+            this.groupBox1.Controls.Add(this.tbClientSecret);
+            this.groupBox1.Controls.Add(this.tbClientID);
+            this.groupBox1.Controls.Add(this.tbClientName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(545, 134);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "App Info";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client Name";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Client ID";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Client Secret";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Redirect URL";
+            // 
+            // tbClientName
+            // 
+            this.tbClientName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClientName.Location = new System.Drawing.Point(95, 17);
+            this.tbClientName.Name = "tbClientName";
+            this.tbClientName.Size = new System.Drawing.Size(444, 20);
+            this.tbClientName.TabIndex = 4;
+            this.tbClientName.Text = "FeaturedImageUpdater";
+            // 
+            // tbClientID
+            // 
+            this.tbClientID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClientID.Location = new System.Drawing.Point(95, 38);
+            this.tbClientID.Name = "tbClientID";
+            this.tbClientID.Size = new System.Drawing.Size(444, 20);
+            this.tbClientID.TabIndex = 5;
+            this.tbClientID.Text = "37262";
+            // 
+            // tbClientSecret
+            // 
+            this.tbClientSecret.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClientSecret.Location = new System.Drawing.Point(95, 59);
+            this.tbClientSecret.Name = "tbClientSecret";
+            this.tbClientSecret.Size = new System.Drawing.Size(444, 20);
+            this.tbClientSecret.TabIndex = 6;
+            this.tbClientSecret.Text = "T9G9QgHVgmdrDgbP3ruTJvrMAcIid8Z1DmvJyXpYAokJRXBZ1oz78QeKMZhCaCpI";
+            // 
+            // tbRedirectUri
+            // 
+            this.tbRedirectUri.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRedirectUri.Location = new System.Drawing.Point(95, 80);
+            this.tbRedirectUri.Name = "tbRedirectUri";
+            this.tbRedirectUri.Size = new System.Drawing.Size(444, 20);
+            this.tbRedirectUri.TabIndex = 7;
+            this.tbRedirectUri.Text = "http://project365.thepotters.org";
+            // 
+            // tbCode
+            // 
+            this.tbCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCode.Location = new System.Drawing.Point(95, 102);
+            this.tbCode.Name = "tbCode";
+            this.tbCode.Size = new System.Drawing.Size(444, 20);
+            this.tbCode.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Code";
+            // 
+            // btnAuthorize
+            // 
+            this.btnAuthorize.Location = new System.Drawing.Point(578, 17);
+            this.btnAuthorize.Name = "btnAuthorize";
+            this.btnAuthorize.Size = new System.Drawing.Size(75, 23);
+            this.btnAuthorize.TabIndex = 1;
+            this.btnAuthorize.Text = "Authorize";
+            this.btnAuthorize.UseVisualStyleBackColor = true;
+            this.btnAuthorize.Click += new System.EventHandler(this.btnAuthorize_Click);
+            // 
+            // browser1
+            // 
+            this.browser1.Location = new System.Drawing.Point(7, 148);
+            this.browser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser1.Name = "browser1";
+            this.browser1.Size = new System.Drawing.Size(545, 415);
+            this.browser1.TabIndex = 2;
+            // 
+            // btnDone
+            // 
+            this.btnDone.Enabled = false;
+            this.btnDone.Location = new System.Drawing.Point(578, 50);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(75, 23);
+            this.btnDone.TabIndex = 3;
+            this.btnDone.Text = "Got It";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // btnCompleteAuth
+            // 
+            this.btnCompleteAuth.Location = new System.Drawing.Point(578, 83);
+            this.btnCompleteAuth.Name = "btnCompleteAuth";
+            this.btnCompleteAuth.Size = new System.Drawing.Size(75, 23);
+            this.btnCompleteAuth.TabIndex = 4;
+            this.btnCompleteAuth.Text = "Complete";
+            this.btnCompleteAuth.UseVisualStyleBackColor = true;
+            this.btnCompleteAuth.Click += new System.EventHandler(this.btnCompleteAuth_Click);
+            // 
+            // rtResponse
+            // 
+            this.rtResponse.Location = new System.Drawing.Point(578, 148);
+            this.rtResponse.Name = "rtResponse";
+            this.rtResponse.ReadOnly = true;
+            this.rtResponse.Size = new System.Drawing.Size(267, 402);
+            this.rtResponse.TabIndex = 5;
+            this.rtResponse.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(886, 403);
+            this.ClientSize = new System.Drawing.Size(886, 649);
+            this.Controls.Add(this.tabControl);
             this.Controls.Add(this.btnGet);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl.ResumeLayout(false);
+            this.tpMain.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +294,25 @@
         #endregion
 
         private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tpMain;
+        private System.Windows.Forms.TabPage tpAuth;
+        private System.Windows.Forms.WebBrowser browser1;
+        private System.Windows.Forms.Button btnAuthorize;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbCode;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbRedirectUri;
+        private System.Windows.Forms.TextBox tbClientSecret;
+        private System.Windows.Forms.TextBox tbClientID;
+        private System.Windows.Forms.TextBox tbClientName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Button btnCompleteAuth;
+        private System.Windows.Forms.RichTextBox rtResponse;
     }
 }
 
--- Wordpress/Form1.cs
+++ Wordpress/Form1.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Configuration;
 using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
 using System.Windows.Forms;
 
 using ServiceStack.ServiceClient.Web;
@@ -34,6 +38,89 @@ namespace Wordpress
 
 
             return;
+        }
+
+        private void btnAuthorize_Click(object sender, EventArgs e)
+        {
+            btnDone.Enabled = true;
+            Application.DoEvents();
+            browser1.Url = new Uri(
+                string.Format(CultureInfo.InvariantCulture,
+                "https://public-api.wordpress.com/oauth2/authorize?client_id={0}&redirect_uri={1}&response_type=code",
+                tbClientID.Text, HttpUtility.UrlEncode(tbRedirectUri.Text)));
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+        //http://project365.thepotters.org/?code=K99chjTnwt&state
+            var fullUri = browser1.Url.AbsoluteUri;
+
+            try
+            {
+                // Gather the bit that we want
+                var parts = fullUri.Split("=".ToCharArray());
+
+                // Part 1 should have everything after the equals sign.  Now get everything before the first &
+                var parts2 = parts[1].Split("&".ToCharArray());
+
+                tbCode.Text = parts2[0];
+            }
+            catch (Exception)
+            {
+                tbCode.Text = "Unexpected reponse format: " + fullUri;
+                throw;
+            }
+        }
+
+        private void btnCompleteAuth_Click(object sender, EventArgs e)
+        {
+            // Build the HTTP Response - note this is POST data, not JSON
+            var request = WebRequest.Create("https://public-api.wordpress.com/oauth2/token") as HttpWebRequest;
+            request.Method = "POST";
+
+            // Build the POST data
+            var postData = string.Format(CultureInfo.InvariantCulture,
+                "client_id={0}&redirect_uri={1}&client_secret={2}&code={3}&grant_type={4}",
+                tbClientID.Text,
+                HttpUtility.UrlEncode(tbRedirectUri.Text),
+                tbClientSecret.Text,
+                tbCode.Text,
+                "authorization_code");
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            // We've built the message, now send it to Wordpress
+            try
+            {
+                var response = request.GetResponse();
+                var responseStream = response.GetResponseStream();
+                var reader = new StreamReader(responseStream);
+                var responseFromServer = HttpUtility.UrlDecode(reader.ReadToEnd());
+
+                // That gives us a message, which we can subsequently decode
+                rtResponse.Text = responseFromServer;
+            }
+            catch (WebException ex)
+            {
+                using (WebResponse webResponse = ex.Response)
+                {
+                    using (var data = webResponse.GetResponseStream())
+                    {
+                        if (null != data)
+                        {
+                            using (var reader = new StreamReader(data))
+                            {
+                                rtResponse.Text = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
